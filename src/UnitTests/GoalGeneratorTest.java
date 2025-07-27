be1315e072406b2fcd3d668f4755b576122fbf12
shivrajbanwait <shivrajbanwait@gmail.com>
2025-07-27 18:20:44 -0400

    Create GoalGeneratorTest.java

---
+++ src/UnitTests/GoalGeneratorTest.java
@@ -0,0 +1,14 @@
+package UnitTests;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class GoalGeneratorTest {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
